2817de70ee7e8a4d2d7061e0cbcd845ba3f398bc
JoaoL-Menezes <joaolucasmeneses51@gmail.com>
2024-04-21 17:58:50 -0300

    update classe cerimonia

--- código/Cerimonia.cs
+++ código/Cerimonia.cs
@@ -1,18 +1,24 @@
 public class Cerimonia
 {
-    private DateTime data 
-    public int quantidadeConvidados 
-    private List<Espaco> espacos 
+    private DateTime data; 
+    public int quantidadeConvidados; 
+    private List<Espaco> espacos; 
 
     public Cerimonia(int quantConvidados, DateTime data, List<Espaco> espacos)
     {
-        quantConvidadosuantidadeConvidados = quantConvidados;
-        data = data;
-        espacos = espacos;
+        this.quantidadeConvidados = quantConvidados;
+        this.data = data;
+        this.espacos = espacos;
     }
-    public bool Prazominimo30dias()
+    public bool VerificaSextaSabado(DateTime data)
     {
-        
+        DayOfWeek dia = data.DayOfWeek;
+        return dia == DayOfWeek.Friday || dia == DayOfWeek.Saturday;
+    }
+    public bool PrazoMinimo30Dias(DateTime data)
+    {
+        double diasNoFuturo = (data - DateTime.Today).TotalDays;
+        return diasNoFuturo >= 30 && VerificaSextaSabado(data);
     }
 
 }

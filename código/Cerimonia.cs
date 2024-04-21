0caee55bde2be2003b6c0a9c3bea54795c85d036
JoaoL-Menezes <joaolucasmeneses51@gmail.com>
2024-04-21 17:36:27 -0300

    update classe cerimonia

--- código/Cerimonia.cs
+++ código/Cerimonia.cs
@@ -1,14 +1,14 @@
 public class Cerimonia
 {
-    private DateTime Data { get; set; }
-    public int QuantidadeConvidados { get; set; }
-    private List<Espaco> Espacos { get; set; }
+    private DateTime data 
+    public int quantidadeConvidados 
+    private List<Espaco> espacos 
 
     public Cerimonia(int quantConvidados, DateTime data, List<Espaco> espacos)
     {
-        QuantidadeConvidados = quantConvidados;
-        Data = data;
-        Espacos = espacos;
+        quantConvidadosuantidadeConvidados = quantConvidados;
+        data = data;
+        espacos = espacos;
     }
     public bool Prazominimo30dias()
     {

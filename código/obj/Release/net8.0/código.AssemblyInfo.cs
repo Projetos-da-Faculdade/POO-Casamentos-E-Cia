fe150acdafa3e0446e1f887c257213ae5b207fc2
thiago-Parreiras <thiagoparreiras25@gmail.com>
2024-05-13 14:40:38 -0300

    CREAT TipoCerimonia

--- código/obj/Release/net8.0/código.AssemblyInfo.cs
+++ código/obj/Release/net8.0/código.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("código")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47319d67f1fb3fa1010c2836fe551077b6e03f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("código")]

a1bb312c2aed029f34d741bc91c7e07f9e95e0a5
ArmintasFilho <armintasfilho@gmail.com>
2024-05-15 14:18:20 -0300

    update

--- código/obj/Debug/net8.0/código.AssemblyInfo.cs
+++ código/obj/Debug/net8.0/código.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("código")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47319d67f1fb3fa1010c2836fe551077b6e03f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("código")]
 [assembly: System.Reflection.AssemblyTitleAttribute("código")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

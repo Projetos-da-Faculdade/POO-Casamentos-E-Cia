377d2e9086c8de11cae4e37ffd20b80e21e2bdfb
arthurtrin117 <arthurtbicalho@hotmail.com>
2024-06-15 11:12:40 -0300

    adição da classe Comida

---
+++ código/Comida.cs
@@ -0,0 +1,11 @@
+private string salgado1;
+private string salgado2;
+private string salgado3;
+private string salgado4;
+
+private double precoSalgados;
+
+public void DefinirPrecoComida()
+{
+    
+}
